05c93fb9c73b00f1db1adb3668c493ee56cea580
GPS4444 <simcraft@outlook.it>
2025-10-17 11:46:43 +0200

    War is over + CharacterController

---
+++ Assets/Scrips/CharacterController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float turnSpeed = 1.0f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.down * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
